7d8f6ef9cddfdd16be23ceca59f924d3e7059592
djmm187 <djmm187@gmail.com>
2013-10-25 14:01:06 -0700

    fixed case issue in column search

--- ticketingsystem/assets/ui/controller/Main.js
+++ ticketingsystem/assets/ui/controller/Main.js
@@ -110,7 +110,7 @@ Ext.define('ui.controller.Main', {
 			store.filter(
 					Ext.create('Ext.util.Filter', {
 						filterFn: function(item) {
-							return item.get(key).match(new RegExp(val, "i")); 
+							return item.get(key.toLowerCase()).match(new RegExp(val, "i")); 
 						}, 
 						root: 'data'
 					})
